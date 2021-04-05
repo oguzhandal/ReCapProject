b4eb1964e52a28c35fafb9c23123294799ed363e
oguzhandal <daloguz11@hotmail.com>
2021-04-05 03:10:43 +0300

    05.04.2021 commit

---
+++ Business/Abstract/ICarImageService.cs
@@ -0,0 +1,19 @@
+﻿using Core.Utilities.Result.Abstract;
+using Entities.Concrete;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace Business.Abstract
+{
+    public interface ICarImageService
+    {
+        IResult Add(IFormFile file, CarImage carImage);
+        IResult Delete(CarImage carImage);
+        IResult Update(IFormFile formFile, CarImage carImage);
+        IDataResult<List<CarImage>> GetAll(Expression<Func<CarImage, bool>> filter = null);
+        IDataResult<CarImage> GetById(int id);
+    }
+}
