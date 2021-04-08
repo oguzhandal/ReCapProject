7ee8703e152c388e7f4e1061e76aebcddffb1680
Oguzhan DAL <55413160+oguzhandal@users.noreply.github.com>
2021-04-09 00:26:55 +0300

    Delete Database directory

--- Database/dbo.Table.sql
+++
@@ -1,8 +0,0 @@
-﻿CREATE TABLE [dbo].[Table]
-(
-	[Id] INT NOT NULL PRIMARY KEY, 
-    [CarId] INT NULL, 
-    [ModelYear] INT NOT NULL, 
-    [DailyPrice] INT NULL, 
-    [Description] NVARCHAR(150) NULL 
-)
